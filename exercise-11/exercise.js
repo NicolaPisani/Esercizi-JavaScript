62a14b671d6984e3c89644948e9cf8b9d731049f
Nicola Pisani <pisani_nicola@yahoo.com>
2023-06-03 00:49:36 +0200

    Esercizio svolto

---
+++ exercise-11/exercise.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  static fromJson(json) {
+    const parsedJson = JSON.parse(json);
+    const { id, firstName, lastName, age } = parsedJson;
+    return new Person(id, firstName, lastName, age);
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+}
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
